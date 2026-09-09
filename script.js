ee68cbf1c78641b44c301988ea9f35bdfa1c0056
Sabrina <sabrinabsouza72@gmail.com>
2026-09-08 22:08:10 -0300

    feat: funcao de decrementar

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const valor = document.querySelector("#valor");
+const botaoDecrementar = document.querySelector("#decrementar");
 const botaoIncrementar = document.querySelector("#incrementar");
 
 let contador = 0;
 
+botaoDecrementar.addEventListener("click", () => {
+    contador -= 1;
+    valor.textContent = contador;
+});
+
 botaoIncrementar.addEventListener("click", () => {
-  contador += 1;
-  valor.textContent = contador;
+    contador += 1;
+    valor.textContent = contador;
 });
